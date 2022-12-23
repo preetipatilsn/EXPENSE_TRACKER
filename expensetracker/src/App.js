032468de8b3b11445d3083fc0e8612f9e2128c96
preetipatilsn <preetipatilsn4@gmail.com>
2022-12-23 13:31:42 +0530

    fetching updated user profile on page refresh

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import MainNavigation from './components/MainNavigation';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate } from 'react-router-dom';
 import UserProfile from './pages/UserProfile';
+import About from './pages/About';
+import Product from './pages/Product';
+import loginContext from './store/login-context';
 
 function App() {
+  const loginCtx = useContext(loginContext);
+
   return (
     <React.Fragment>
       <MainNavigation />
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/' exact element={<Navigate replace to='/home' />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/profile' element={<UserProfile />}/>
+
+        {loginCtx.isLoggedIn ? (
+          <Route path='/product' element={<Product />} />
+        ) : (
+          <Route path='/product' element={<Navigate replace to='/login' />} />
+        )}
+
+        <Route path='/about' element={<About />} />
+
+        {loginCtx.isLoggedIn ? (
+          <Route path='/profile' element={<UserProfile />} />
+        ) : (
+          <Route path='/profile' element={<Navigate replace to='/login' />} />
+        )}
+
+        <Route path='/login' element={<Login />} />
       </Routes>
     </React.Fragment>
   );
--- expensetracker/src/components/UserProfileForm.js
+++ expensetracker/src/components/UserProfileForm.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from 'react';
+import React, { useRef, useContext,useEffect,useCallback} from 'react';
 
 import classes from './UserProfileForm.module.css';
 import loginContext from '../store/login-context';
@@ -20,16 +20,19 @@ const UserProfileForm = () => {
             body: JSON.stringify({
               idToken: localStorage.getItem('idToken'),
               displayName: nameRef.current.value,
-              imageUrl: imageRef.current.value,
+              photoUrl: imageRef.current.value,
               returnSecureToken: true,
             }),
             headers: {
               'Content-Type': 'application/json',
             },
           }
-        );
+          );
+          
+          const data = await res.json();
 
-        if (res.ok) {
+          if (res.ok) {
+              console.log(data);
           nameRef.current.value = '';
           imageRef.current.value = '';
           const data = await res.json();
@@ -44,22 +47,59 @@ const UserProfileForm = () => {
     }
   };
 
+  const getUserProfile = useCallback (async () => {
+    try {
+      // console.log('called');
+      const res = await fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDncz8xuHCf97stm52YbFh9jvASdOqjLC4',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            idToken: localStorage.getItem('idToken'),
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      const data = await res.json();
+      if (res.ok) {
+        nameRef.current.value = data.users[0].displayName;
+        imageRef.current.value = data.users[0].photoUrl;
+      } else {
+        throw data.error;
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  },[]);
+
+
+  const { isLoggedIn } = loginCtx;
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      getUserProfile();
+    }
+  }, [getUserProfile, isLoggedIn]);
+
   return (
-      <form className={classes.form} onSubmit={profileSubmitHandler}>
-        <div className={classes.formHead}>
-          <span>Contact Details</span>
-          <button>Cancel</button>
+    <form className={classes.form} onSubmit={profileSubmitHandler}>
+      <div className={classes.formHead}>
+        <span>Contact Details</span>
+        <button>Cancel</button>
+      </div>
+      <div className={classes.formBody}>
+        <label>Full Name:</label>
+        <input type='text' ref={nameRef} />
+        <label>Profile Photo URL:</label>
+        <input type='text' ref={imageRef} />
+        <div className={classes.button}>
+          <button type='submit'>Update</button>
         </div>
-        <div className={classes.formBody}>
-          <label>Full Name:</label>
-          <input type='text' ref={nameRef} />
-          <label>Profile Photo URL:</label>
-          <input type='text' ref={imageRef} />
-          <div className={classes.button}>
-            <button type='submit'>Update</button>
-          </div>
-        </div>
-      </form>
+      </div>
+    </form>
   );
 };
 
